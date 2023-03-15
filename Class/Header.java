183279be6cf70d75756ac9fc045e1ab9a63e3cb5
Rismayanti <93065075+chimzy11@users.noreply.github.com>
2023-03-15 13:33:57 +0000

    Add files via upload

---
+++ Class/Header.java
@@ -0,0 +1,13 @@
+package Class;
+import java.io.IOException;
+
+public class Header {
+    public void header()  throws IOException{
+        System.out.println("---------------------------------------------------------------------------------------------------------------");
+            System.out.println("                   S I S T E M || P E N G E L O L A A N || L U K I S A N || G A L L E R Y " );
+            System.out.println((char)27 +  "[01;35m \n.....   .  .   ....       ....   ....   ....   ....   .....   ....   ....   .....       .   .   .   ....   .  .");
+            System.out.println(                   "  :     :..:   :...   -   : ..   :..:   :...   :..:     :     :...   :...     :     -   : . :   :   :...   :..:");
+            System.out.println(                     "  :     :  :   :...       :..:   :  .   :...   :  :     :     :...   ...:     :         .: :.   :   ...:   :  :"+ (char)27 + "[00;00m");
+            System.out.println("---------------------------------------------------------------------------------------------------------------");
+    }
+}
